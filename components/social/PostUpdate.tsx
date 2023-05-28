ac714b11780801fc8684567fb7e27e41fedf00ad
Nathan Englert <nenglert@gmail.com>
2023-05-27 20:44:07 -0500

    Add randomized prompts to post component

--- components/social/PostUpdate.tsx
+++ components/social/PostUpdate.tsx
@@ -1,15 +1,43 @@
+import { useState, useEffect } from "react";
 import { CameraIcon, PencilSquareIcon } from "../Icons";
 import { Button } from "../ui/Button";
 import Heading from "../ui/Heading";
 import { TextArea } from "../ui/TextArea";
 
+const prompts = [
+  "Who died?",
+  "What is your blood pressure?",
+  "What are you wearing?",
+  "What did you do today?",
+  "Where are you right now?",
+];
+
 export function PostUpdate() {
+  const timeoutMinutes = 1;
+
+  const [placeholder, setPlaceholder] = useState(() => {
+    return prompts[Math.floor(Math.random() * prompts.length)];
+  });
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setPlaceholder(() => {
+        return prompts[Math.floor(Math.random() * prompts.length)];
+      });
+    }, timeoutMinutes * 60 * 1000);
+
+    // Clean up function
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [timeoutMinutes]);
+
   return (
     <div>
       <Heading level={5}>
         <PencilSquareIcon /> Post an Update
       </Heading>
-      <TextArea name="update" placeholder="Who died?" />
+      <TextArea name="update" placeholder={placeholder} />
       <div className="flex w-full justify-end space-x-3 mt-1">
         <Button>
           <CameraIcon /> Add Photo
